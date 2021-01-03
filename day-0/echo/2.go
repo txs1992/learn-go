f8db5fc816c720a9f90cc459504f02416e70e643
txs1992 <xusheng.txs@gmail.com>
2021-01-03 11:05:09 +0800

    feature：add echo

---
+++ day-0/echo/2.go
@@ -0,0 +1,16 @@
+// echo 2 输出命令行参数
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
---
+++ day-0/echo/demo.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(os.Args[0])
+	
+	for i := 0; i < os.args
+}
