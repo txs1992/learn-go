eb32980c965c0dbfbb56ea2349394f87af027520
txs1992 <xusheng.txs@gmail.com>
2021-01-21 08:30:49 +0800

    feature：完成字符串练习

---
+++ day-07/comma.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Printf("%s\n", comma("12345678"))
+}
+
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s;
+	}
+	return comma(s[:n-3]) + "," + s[n-3:]
+}
