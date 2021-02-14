4e6ef3ed89444bfbe0f822d0adfdef2b9594bc9b
txs1992 <xusheng.txs@gmail.com>
2021-02-14 11:36:11 +0800

    feature：练习。

--- day-07/comma.go
+++ day-07/comma.go
@@ -1,15 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "bytes"
+	"strconv"
+)
 
 func main()  {
-	fmt.Printf("%s\n", comma("12345678"))
+	fmt.Printf("%s\n", comma(123.4))
 }
 
-func comma(s string) string {
-	n := len(s)
-	if n <= 3 {
-		return s;
-	}
-	return comma(s[:n-3]) + "," + s[n-3:]
+func comma(f float64) string {
+	// var buf bytes.Buffer
+	substr := strconv.FormatFloat(f,'G', -1, 64)
+	return substr
+	// for i := len(s) - 1; i >=0; i-- {
+
+	// }
+	
+	// n := len(s)
+	// if n <= 3 {
+	// 	return s;
+	// }
+	// return comma(s[:n-3]) + "," + s[n-3:]
 }
